8b9eade377c90e88b8656c3b7f48ff503642fe8d
william <williamimoh@yahoo.com>
2024-09-29 03:31:05 +0200

    fix blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,21 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import remarkToc from "remark-toc";
+import rehypePresetMinify from "rehype-preset-minify";
+import rehypeSlug from "rehype-slug";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx()]
+  integrations: [
+    react(),
+    tailwind(),
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: { theme: "dracula" },
+      remarkPlugins: [remarkToc],
+      rehypePlugins: [rehypePresetMinify, rehypeSlug],
+      remarkRehype: { footnoteLabel: "Footnotes" },
+    }),
+  ],
 });
--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -4,7 +4,7 @@ export const Article = ({ frontmatter }) => {
   return (
     <div className="flex justify-center bg-bgDark2 relative pt-2">
       <div className="px-2 sm:px-4">
-        <article className=" p-8 text-center rounded-3xl w-full lg:w-[1200px] 2xl:w-[1400px] mb-24 mt-16 sm:mt-24">
+        <article className="p-8 text-center rounded-3xl w-full lg:w-[1200px] 2xl:w-[1400px] mt-16 sm:mt-24">
           <header className="mb-12">
             <div className="text-sm text-secondaryText my-4">{date}</div>
             <h1 className="text-[2rem] xs:text-[2.8rem] lg:text-[3.5rem] font-bold text-primaryText mb-4">
@@ -12,12 +12,6 @@ export const Article = ({ frontmatter }) => {
             </h1>
             <p className="text-lg text-secondaryText mb-4">{subtitle}</p>
           </header>
-          <img
-            src={image}
-            alt={title}
-            className="rounded-3xl mb-8 w-10/12 md:w-3/4 mx-auto mt-8"
-            aria-label={title}
-          />
         </article>
       </div>
     </div>
